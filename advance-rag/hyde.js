af36744efe615f2e9493815dc2b20af32ff261e1
rathodvinod5 <rathodvinod5@gmail.com>
2026-08-19 20:05:21 +0530

    HyDE technic

---
+++ advance-rag/hyde.js
@@ -0,0 +1,56 @@
+import { MemoryVectorStore } from "@langchain/classic/vectorstores/memory";
+import { Document } from "@langchain/core/documents";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import "dotenv/config";
+
+const rawDocs = [
+  new Document({
+    pageContent: `To resolve an HTTP 404 error on Nginx,
+          verify your root directive in nginx.conf matches your actual build directory path`,
+    metadata: { id: 1 },
+  }),
+  new Document({
+    pageContent: `Node.js memory leaks can often be tracked using the heapdump module or 
+    inspecting the V8 heap profile via Chrome DevTools.`,
+    metadata: { id: 2 },
+  }),
+];
+
+async function main() {
+  const embedding = new OpenAIEmbeddings({
+    model: "text-embedding-3-small",
+  });
+  const llm = new ChatOpenAI({
+    model: "gpt-4o-mini",
+    temperature: 0,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const vectorStore = await MemoryVectorStore.fromDocuments(rawDocs, embedding);
+  const baseRetriever = vectorStore.asRetriever({ k: 1 });
+  const hydePrompt = ChatPromptTemplate.fromTemplate(
+    `Please write a short, highly technical paragraph ansering this question.
+    Do not say 'Here is the answer:', just write the answer directly
+    
+    Question:
+    {question}`,
+  );
+  const query =
+    "My nginx server is showing a missing page error, how do I configure the path?";
+  const hydePromptChain = await RunnableSequence.from([
+    hydePrompt,
+    llm,
+    new StringOutputParser(),
+  ]);
+  const hydeDoc = await hydePromptChain.invoke({
+    question: query,
+  });
+  console.log("HydeDoc: ", hydeDoc);
+  //   const result = await vectorStore.similaritySearch(hydeDoc);
+  const result = await baseRetriever.invoke(hydeDoc);
+  console.log("Result: ", result);
+}
+
+main().catch((error) => console.log("Err: ", error));
